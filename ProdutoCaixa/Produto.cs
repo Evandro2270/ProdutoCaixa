1b63b0d07bb8f52290cdf2de62a03bca0ab315a1
Evandro2270 <evandro535@gmail.com>
2024-07-25 09:17:16 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutoCaixa/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutoCaixa
+{
+
+    internal class Produto
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque(){
+            return Preco * Quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ "
+                + Preco.ToString("F2", CI)
+                +", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CI);
+        }
+    }
+}
---
+++ ProdutoCaixa/Program.cs
@@ -0,0 +1,28 @@
+﻿using ProdutoCaixa;
+using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                Produto p = new Produto();
+
+                Console.WriteLine("Entre com dados do produto: ");
+                Console.Write("Nome: ");
+                p.Nome = Console.ReadLine();
+                Console.Write("Preço: ");
+                p.Preco = double.Parse(Console.ReadLine(), CI);
+                Console.Write("Quantidade no estoque: ");
+                p.Quantidade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Dados do produto " + p);
+            }
+        }
+    }
+}
